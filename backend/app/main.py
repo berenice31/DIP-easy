ea646bff8d70cde8ed0534720aa00e1b107de18e
bérénice lortie <berenice.lortie@gmail.com>
2025-06-24 16:21:31 +0200

    chore: add dipeasy.com to CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,6 +17,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://dipeasy.netlify.app",
+    "https://dipeasy.com",
+    "https://www.dipeasy.com",
 ]
 
 app.add_middleware(
